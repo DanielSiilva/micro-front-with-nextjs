6260f602a797b71a6d6fce7bda7e639894489c8d
DanielSiilva <daniellsousa0614@gmail.com>
2024-04-08 14:33:47 -0300

    feat: init

---
+++ micro-front-end-activate/components/mario.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "../styles/Mario.module.css";
+
+const Mario = () => {
+  return (
+    <main className={styles.main}>
+      <p>App 01</p>
+    </main>
+  );
+};
+
+export default Mario;
---
+++ micro-front-end-main/components/luigi.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "../styles/Luigi.module.css";
+
+const Luigi = () => {
+  return (
+    <main className={styles.main}>
+      <p>App 02</p>
+    </main>
+  );
+};
+
+export default Luigi;
